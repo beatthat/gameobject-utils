6dc454c0181d1cd3521fd707f628f281417a06c0
larrykirschner <larrykirschner@gmail.com>
2017-09-14 12:59:12 -0700

    0.0.3

---
+++ src/gameobject-utils/SetInactive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace BeatThat
+{
+	/// <summary>
+	/// Sets some remote GameObject inactive in OnEnable
+	/// </summary>
+	public class SetInactive : MonoBehaviour
+	{
+		public GameObject m_target;
+
+		void OnEnable()
+		{
+			m_target.SetActive(false);
+		}
+
+
+	}
+}
